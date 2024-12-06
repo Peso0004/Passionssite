a89a48dacbcb994ec39fda31dc096b222d0ed853
Peso0004 <peso0004@stud.kea.dk>
2024-12-06 14:09:36 +0100

    init

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("burgerMenu").addEventListener("click", function () {
+  const menu = document.getElementById("menu");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+});
